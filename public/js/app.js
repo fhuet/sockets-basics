159e737a835c77463dfab4a9af60fbfae6048ba6
flexadm <fhuet@2xcel-us.com>
2017-02-10 12:31:38 -0800

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,20 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function() {
     console.log('Connected to the server');
 });
 
 socket.on('message', function(message) {
+    var tsMoment = moment.utc(message.timestamp).local().format('h:mm a');
+    var $message = jQuery('.messages');
     console.log('New message:');
     console.log(message.timestamp, message.text);
-    var tsMoment = moment.utc(message.timestamp).local().format('h:mm a');
-
-    jQuery('.messages').append('<p><strong>' + tsMoment + '</strong> : ' + message.text + '</p>');
+    $message.append('<p><strong>' + message.name + '  ' + tsMoment + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');
 
 });
 
@@ -20,6 +25,7 @@ $form.on('submit', function(event) {
     event.preventDefault();
     var $message = $form.find('input[name=message]');
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
     $message.val('');
